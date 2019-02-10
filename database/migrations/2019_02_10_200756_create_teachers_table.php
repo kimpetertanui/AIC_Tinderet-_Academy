7eea5e011b1e1d29f742017b4ad60151b20b95e1
starno <starnleymbote@gmail.com>
2019-02-10 23:19:53 +0300

    Created the teachers model and made an update to the students models

--- database/migrations/2019_02_10_200756_create_teachers_table.php
+++ database/migrations/2019_02_10_200756_create_teachers_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateStudentsTable extends Migration
+class CreateTeachersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,12 @@ class CreateStudentsTable extends Migration
      */
     public function up()
     {
-        Schema::create('students', function (Blueprint $table) {
+        Schema::create('teachers', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('First_Name');
-            $table->string('Last_Name');
-            $table->string('Admission_number')->unique();
-            $table->string('Year_Admitted');
+            $table->string('First_name');
+            $table->string('Last_name');
+            $table->string('Teachers_no');
+            $table->string('email')->unique();
             $table->timestamps();
         });
     }
@@ -30,6 +30,6 @@ class CreateStudentsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('students');
+        Schema::dropIfExists('teachers');
     }
 }
